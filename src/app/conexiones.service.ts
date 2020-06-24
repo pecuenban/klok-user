e69e8fc70fa2422a5b753deae403457b3196f9a6
pecuenban <pedrocuenca95@yahoo.es>
2020-06-24 02:23:23 +0200

    Servidor europa

--- src/app/conexiones.service.ts
+++ src/app/conexiones.service.ts
@@ -17,39 +17,39 @@ export class ConexionesService {
     localStorage.setItem("Finca", finca);
   }
   getFinca(id){
-    return this.http.get('https://us-central1-klok-reservas.cloudfunctions.net/api/finca/'+id, this.httpOptions);
+    return this.http.get('https://europe-west2-klok-reservas.cloudfunctions.net/api/finca/'+id, this.httpOptions);
   }
    postFinca(finca){
-    return this.http.post('https://us-central1-klok-reservas.cloudfunctions.net/api/finca',finca, this.httpOptions);
+    return this.http.post('https://europe-west2-klok-reservas.cloudfunctions.net/api/finca',finca, this.httpOptions);
   }
    putSala(id,sala){
-    return this.http.put('https://us-central1-klok-reservas.cloudfunctions.net/api/sala/'+id,sala, this.httpOptions);
+    return this.http.put('https://europe-west2-klok-reservas.cloudfunctions.net/api/sala/'+id,sala, this.httpOptions);
   }
    postSala(id,sala){
-    return this.http.post('https://us-central1-klok-reservas.cloudfunctions.net/api/sala/'+id,sala, this.httpOptions);
+    return this.http.post('https://europe-west2-klok-reservas.cloudfunctions.net/api/sala/'+id,sala, this.httpOptions);
   }
    autenticarUser(user){
-    return this.http.post('https://us-central1-klok-reservas.cloudfunctions.net/api/autenticar/user',user, this.httpOptions);
+    return this.http.post('https://europe-west2-klok-reservas.cloudfunctions.net/api/autenticar/user',user, this.httpOptions);
   }
 
   getHoras(finca,id,fecha){
-    return this.http.get('https://us-central1-klok-reservas.cloudfunctions.net/api/horas/'+finca+"/"+id+"/"+fecha, this.httpOptions);
+    return this.http.get('https://europe-west2-klok-reservas.cloudfunctions.net/api/horas/'+finca+"/"+id+"/"+fecha, this.httpOptions);
   }
 
   getReservas(id):Observable<any>{
-    return this.http.get('https://us-central1-klok-reservas.cloudfunctions.net/api/reserva/'+id, this.httpOptions);
+    return this.http.get('https://europe-west2-klok-reservas.cloudfunctions.net/api/reserva/'+id, this.httpOptions);
   }
   
   reservar(finca,sala,reserva){
-    return this.http.post('https://us-central1-klok-reservas.cloudfunctions.net/api/reserva/'+finca+"/"+sala, reserva,this.httpOptions);
+    return this.http.post('https://europe-west2-klok-reservas.cloudfunctions.net/api/reserva/'+finca+"/"+sala, reserva,this.httpOptions);
   }
   
   check(qr,user):Observable<any>{
-    return this.http.put('https://us-central1-klok-reservas.cloudfunctions.net/api/check/'+qr,user,this.httpOptions);
+    return this.http.put('https://europe-west2-klok-reservas.cloudfunctions.net/api/check/'+qr,user,this.httpOptions);
   }
 
  elimiarReserva(id,user):Observable<any>{
-    return this.http.delete('https://us-central1-klok-reservas.cloudfunctions.net/api/eliminar/'+id+"/"+user,this.httpOptions);
+    return this.http.delete('https://europe-west2-klok-reservas.cloudfunctions.net/api/eliminar/'+id+"/"+user,this.httpOptions);
   }
 
   
