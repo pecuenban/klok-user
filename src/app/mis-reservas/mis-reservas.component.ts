a8d98c1e15e08bbd355e5c0d2967cfb0c7c32157
pecuenban <pedrocuenca95@yahoo.es>
2020-07-10 09:47:31 +0200

    Menu e imagenes

--- src/app/mis-reservas/mis-reservas.component.ts
+++ src/app/mis-reservas/mis-reservas.component.ts
@@ -11,6 +11,8 @@ export class MisReservasComponent implements OnInit {
   constructor(protected conexionesService: ConexionesService) { }
 reserva:any;
 finca;
+idEliminar;
+popUp = false;
   ngOnInit() {
     
  this.conexionesService.getReservas(JSON.parse(localStorage.getItem("User")).Id)
@@ -38,17 +40,27 @@ finca;
 
   }
 
-eliminar(id){
+  confirmacion(id){
+    this.idEliminar = id;
+    this.popUp = true;
+  }
   
-   this.conexionesService.elimiarReserva(id,JSON.parse(localStorage.getItem("User")).Id)
+  cerrar(){
+    this.popUp = false;
+  }
+
+eliminar(){
+  
+   this.conexionesService.elimiarReserva(this.idEliminar,JSON.parse(localStorage.getItem("User")).Id)
     .subscribe(
       (data) => { // Success
       //  this.finca = data;
         console.log(data)
         if(data.msg == "ok"){
           for(var i = 0; i < this.reserva.length;i++){
-            if(this.reserva[i].Id == id){
+            if(this.reserva[i].Id == this.idEliminar){
               this.reserva.splice(i,1);
+              this.popUp = false;
             }
           }
         }
